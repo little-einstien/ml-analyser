3e81881676fdcaf4f6522535e83a636a3a825788
little-einstien <arnavsimer@ymail.com>
2021-10-05 19:20:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-mqdhik
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mqdhik)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, VERSION } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public path = '';
+  public label = '';
+  public confidence = '';
+  public loading = false;
+  constructor(private apiService: ApiService) {}
+  analyse() {
+    this.loading = true;
+    this.apiService
+      .post('https://0j9med1ynl.execute-api.us-east-1.amazonaws.com/prod', {
+        path: this.path,
+      })
+      .subscribe((resp: any) => {
+        console.log(resp);
+        this.label =
+          resp.body.CustomLabels[0].Name == 'dirty'
+            ? 'dirty floor'
+            : resp.body.CustomLabels[0].Name;
+        this.confidence = resp.body.CustomLabels[0].Confidence;
+        this.path = '';
+        this.loading = false;
+      });
+  }
+}
